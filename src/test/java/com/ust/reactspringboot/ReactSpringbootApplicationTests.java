0fdee2bd8a4506a8cff8ba16770ac58f7c99cd0d
Vishal Rane <vishalsrane@gmail.com>
2021-04-20 12:39:33 +0530

    first commit

---
+++ src/test/java/com/ust/reactspringboot/ReactSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.reactspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
